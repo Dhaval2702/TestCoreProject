f7acf18181d3434b3ce6a402ff658ccf3cb9e326
Rathod, Jayesh <mailto:jrathod@cvent.com>
2019-09-04 17:41:10 +0530

    Updated code for 6869 branch

--- TestNewApplication/TestNewApplication/Program.cs
+++ TestNewApplication/TestNewApplication/Program.cs
@@ -6,7 +6,7 @@ namespace TestNewApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World This is TFT-6868 for Email Implementation New Branch for Working Copy Test@@@@@ !");
+            Console.WriteLine("Hello World This is TFT-6868 for Email Implementation New Branch for Working Copy Test@@@@@   This is 6869 Branch Code !");
         }
     }
 }
